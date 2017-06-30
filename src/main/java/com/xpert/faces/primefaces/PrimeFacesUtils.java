44d9786e51aec58945e3a2e19a14a00b659374c9
ayslanms <ayslanms@gmail.com>
2017-06-30 10:37:26 -0300

    PrimeFacesUtils compatible with primefaces 3,4,5 and 6

--- src/main/java/com/xpert/faces/primefaces/PrimeFacesUtils.java
+++ src/main/java/com/xpert/faces/primefaces/PrimeFacesUtils.java
@@ -1,21 +1,25 @@
 package com.xpert.faces.primefaces;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import javax.faces.FacesException;
+import org.primefaces.context.ApplicationContext;
 import org.primefaces.context.RequestContext;
 
 /**
  * Util class to Primefaces
- * 
+ *
  * @author ayslan
  */
 public class PrimeFacesUtils {
 
     /**
      * Primefaces 4 and 5 new widget var is like "PF('widgetVar')"
-     * 
+     *
      * @param widgetVar
      * @param primeFacesVersion
-     * @return 
+     * @return
      */
     public static String normalizeWidgetVar(String widgetVar, PrimeFacesVersion primeFacesVersion) {
         if (primeFacesVersion.isUsePFWidgetVar()) {
@@ -58,19 +62,61 @@ public class PrimeFacesUtils {
     private static Boolean IS_VERSION_3;
 
     public static boolean isVersion4() {
-        if(isVersion3()){
+        if (isVersion3()) {
             return false;
         }
-        return RequestContext.getCurrentInstance().getApplicationContext().getConfig().getBuildVersion().startsWith("4");
+
+        return getPrimefacesBuildVersion().startsWith("4");
     }
-    
+
+    /**
+     * This method get by reflection primefaces build version.
+     *
+     * The reason why use reflection is:
+     *
+     * Primefaces 3 uses org.primefaces.util.Constants.
+     *
+     * Primefaces 4,5, and 6 uses
+     * RequestContext.getCurrentInstance().getApplicationContext().getConfig().getBuildVersion().
+     *
+     * However in primefaces 4 and 5 getConfig() returns
+     * org.primefaces.config.ConfigConteiner and primefaces 6 returns
+     * org.primefaces.config.PrimeConfiguration.
+     *
+     *
+     * @return
+     */
+    public static String getPrimefacesBuildVersion() {
+        ApplicationContext context = RequestContext.getCurrentInstance().getApplicationContext();
+        try {
+            
+            //acess getConfig()
+            Method methodGetConfig = context.getClass().getDeclaredMethod("getConfig");
+            Object config = methodGetConfig.invoke(context);
+            //acess getBuildVersion()
+            Method methodGetBuildVersion = config.getClass().getDeclaredMethod("getBuildVersion");
+            String buildVersion = (String) methodGetBuildVersion.invoke(config);
+            return buildVersion;
+        } catch (NoSuchMethodException ex) {
+            throw new FacesException(ex);
+        } catch (SecurityException ex) {
+            throw new FacesException(ex);
+        } catch (IllegalAccessException ex) {
+            throw new FacesException(ex);
+        } catch (IllegalArgumentException ex) {
+            throw new FacesException(ex);
+        } catch (InvocationTargetException ex) {
+            throw new FacesException(ex);
+        }
+    }
+
     public static boolean isVersion5() {
-        if(isVersion3()){
+        if (isVersion3()) {
             return false;
         }
-        return RequestContext.getCurrentInstance().getApplicationContext().getConfig().getBuildVersion().startsWith("5");
+        return getPrimefacesBuildVersion().startsWith("5");
     }
-    
+
     public static boolean isVersion3() {
         if (IS_VERSION_3 != null) {
             return IS_VERSION_3;
