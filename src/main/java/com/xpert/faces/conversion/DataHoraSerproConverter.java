ffbdcb97be95c48b8c4d923e606b26a23c0a6a30
thiegoramos <thiego.gustavo@gmail.com>
2023-02-28 23:01:55 -0300

    Update DataHoraSerproConverter.java

--- src/main/java/com/xpert/faces/conversion/DataHoraSerproConverter.java
+++ src/main/java/com/xpert/faces/conversion/DataHoraSerproConverter.java
@@ -40,7 +40,28 @@ public class DataHoraSerproConverter implements Converter {
                 SimpleDateFormat dt = new SimpleDateFormat("dd/MM/yyyy HH:mm");
                 return dt.format(dataHora);
             } catch (ParseException ex) {
-                return null;
+                try {
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+                    Date dataHora = sdf.parse(dataPadraoSerpro);
+                    SimpleDateFormat dt = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+                    return dt.format(dataHora);
+                } catch (ParseException ex2) {
+                    try {
+                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.S");
+                        Date dataHora = sdf.parse(dataPadraoSerpro);
+                        SimpleDateFormat dt = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+                        return dt.format(dataHora);
+                    } catch (ParseException ex3) {
+                        try {
+                            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                            Date dataHora = sdf.parse(dataPadraoSerpro);
+                            SimpleDateFormat dt = new SimpleDateFormat("dd/MM/yyyy");
+                            return dt.format(dataHora);
+                        } catch (ParseException ex4) {
+                            return null;
+                        }
+                    }
+                }
             }
         }
     }
