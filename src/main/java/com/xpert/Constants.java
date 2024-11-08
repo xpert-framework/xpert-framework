d594ac17945ea930da60166ce88b41bb1364a090
Arnaldo Junior <arnaldofox@gmail.com>
2024-11-08 17:26:09 -0300

    Start development of 2.0.1-SNAPSHOT

--- src/main/java/com/xpert/Constants.java
+++ src/main/java/com/xpert/Constants.java
@@ -6,7 +6,7 @@ package com.xpert;
  */
 public class Constants {
 
-    public static final String VERSION = "2.0.0";
+    public static final String VERSION = "2.0.1-SNAPSHOT";
     public static final String USER_ROLES = "userRoles";
     public static final String USER_ROLES_KEY_MAP = "userRolesKeyMap";
     public static final String USER_ROLES_URL_MAP = "userRolesUrlMap";
