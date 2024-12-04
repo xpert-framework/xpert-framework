a4401b47147284004cdd2fe1e240d0dc2d6896d2
Arnaldo Junior <arnaldofox@gmail.com>
2024-12-04 11:32:50 -0300

    Ajustes para a tag 1.9.2

--- src/main/java/com/xpert/Constants.java
+++ src/main/java/com/xpert/Constants.java
@@ -6,7 +6,7 @@ package com.xpert;
  */
 public class Constants {
 
-    public static final String VERSION = "1.9.0-SNAPSHOT";
+    public static final String VERSION = "1.9.2";
     public static final String USER_ROLES = "userRoles";
     public static final String USER_ROLES_KEY_MAP = "userRolesKeyMap";
     public static final String USER_ROLES_URL_MAP = "userRolesUrlMap";
